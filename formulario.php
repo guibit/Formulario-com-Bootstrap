c12e750e744b1747d13e130349b8d60aa8ba5854
Guilherme Schutz Leite <53975720+guibit@users.noreply.github.com>
2021-05-01 12:24:20 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,3 +1,13 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$nascimento = $_POST["nascimento"];
+$estado = $_POST["estado"];
+$endereco = $_POST["endereco"];
+$sexo = $_POST["sexo"];
+$cartao = $_POST["cartao"];
+
+echo "Nome: ". $nome ."</br>\n E-mail: ". $email . "\n </br> Data de Nascimento: ". $nascimento . "\n </br>Estado: ". $estado, "\n </br>Endereço: ". $endereco,"\n</br> Sexo: ". $sexo."\n</br> Tipo de cartão:". $cartao ;
 
 ?>
